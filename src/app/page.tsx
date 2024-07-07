32324351fecccaaccfa1fb35768d25be0868707a
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-07 19:17:28 +0530

    create logo ticker animation effect

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import LogoTicker from "@/components/LogoTicker";
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-[20rem] w-96 mx-auto">
+     
+      <LogoTicker />
+     
+    </div>
+  );
+}
